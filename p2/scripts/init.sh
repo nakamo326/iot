f132141eb2be42208be38684929b06b0fd6f9b44
nakamo326 <nakamo326@gmail.com>
2023-04-29 19:04:17 +0900

    prometheusのingress設定

--- p2/scripts/init.sh
+++ p2/scripts/init.sh
@@ -2,6 +2,7 @@
 
 # instal k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=enp0s8" sh -
+# vagrnt userからクラスターにアクセスできるようにreadの権限を付与
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
@@ -22,16 +23,17 @@ ret=$?
 done
 
 # install helm
-# curl -O https://get.helm.sh/helm-v3.11.2-linux-amd64.tar.gz 
-# tar -zxvf helm-v3.11.2-linux-amd64.tar.gz
-# mv linux-amd64/helm /usr/local/bin/helm
+curl -O https://get.helm.sh/helm-v3.11.2-linux-amd64.tar.gz 
+tar -zxvf helm-v3.11.2-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
 
 # install prometheus
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo update
-# helm install prometheus prometheus-community/prometheus
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/prometheus -f /vagrant/k8s/prometheus.yaml
 
 # # install grafana
 # helm repo add grafana https://grafana.github.io/helm-charts
 # helm repo update
 # helm install grafana grafana/grafana -f /vagrant/k8s/grafana.yaml
+# kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo > /vagrant/grafana_password
