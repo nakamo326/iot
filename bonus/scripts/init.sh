e46395407a32f76a322f0381440149670a090c1d
Yasufumi Nakamoto <nakamo326@gmail.com>
2023-05-03 01:24:17 +0900

    fix init.sh

--- bonus/scripts/init.sh
+++ bonus/scripts/init.sh
@@ -8,11 +8,7 @@ mgn="\033[35m"
 cyn="\033[36m"
 nc="\033[m"
 
-# install k3d, please input your password
-echo -e "${grn}install k3d command.${nc}"
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
 # install repo
 echo -e "${grn}add helm repository.${nc}"
-helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add gitlab https://charts.gitlab.io
 helm repo update
